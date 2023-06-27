95027a53914003281b0e1d54f050f071e59cd705
haruto17 <s1902272@sendai-nct.jp>
2023-06-27 16:37:13 +0900

    使わないcssの削除

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Checkbox, Text, Input, Flex, Button, List } from "@mantine/core";
 import { IconTrash } from "@tabler/icons-react";
-import "./TodoList.css";
 
 function TodoList() {
     const initialState = [
