d816dcd448c367dc6710c544488108bbe7cc7ea8
haruto17 <s1902272@sendai-nct.jp>
2023-06-27 17:52:22 +0900

    cssファイルの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import "./App.css";
 import { MantineProvider } from "@mantine/core";
 import TodoList from "./TodoList";
 
 function App() {
     return (
-        <div className="todolist">
-            <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles withNormalizeCSS>
-                <div>
-                    <TodoList />
-                </div>
-            </MantineProvider>
-        </div>
+        <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles withNormalizeCSS>
+            <TodoList />
+        </MantineProvider>
     );
 }
 
