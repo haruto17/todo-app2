12b4ea3e6a125ea1009748c920d339e4c910168b
haruto17 <s1902272@sendai-nct.jp>
2023-06-27 17:32:07 +0900

    テーマを強制。ダークモード切替ボタンを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,15 @@
-import { useState } from "react";
 import "./App.css";
+import { MantineProvider } from "@mantine/core";
 import TodoList from "./TodoList";
-import {
-    MantineProvider,
-    ColorScheme,
-    ColorSchemeProvider,
-    ActionIcon,
-    useMantineColorScheme,
-    Group,
-} from "@mantine/core";
-import { IconSun, IconMoonStars } from "@tabler/icons-react";
-
-function Demo() {
-    const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-
-    return (
-        <Group position="left" my="xl">
-            <ActionIcon
-                onClick={() => toggleColorScheme()}
-                size="lg"
-                sx={(theme) => ({
-                    backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
-                    color: theme.colorScheme === "dark" ? theme.colors.yellow[4] : theme.colors.blue[6],
-                })}
-            >
-                {colorScheme === "dark" ? <IconSun size="1.2rem" /> : <IconMoonStars size="1.2rem" />}
-            </ActionIcon>
-        </Group>
-    );
-}
 
 function App() {
-    const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
-    const toggleColorScheme = (value?: ColorScheme) =>
-        setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
     return (
         <div className="todolist">
-            <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-                <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-                    <Demo />
-                </MantineProvider>
-            </ColorSchemeProvider>
-            <div>
-                <TodoList />
-            </div>
+            <MantineProvider theme={{ colorScheme: "dark" }} withGlobalStyles withNormalizeCSS>
+                <div>
+                    <TodoList />
+                </div>
+            </MantineProvider>
         </div>
     );
 }
